83e19ab20227b3a5d838b6b73ab34e8426e50eb6
valterj <56932864+valterj@users.noreply.github.com>
2021-01-17 19:09:19 -0300

    Ordens de Serviço

---
+++ src/main/java/com/algaworks/osworks/domain/model/SituacaoOrdemServico.java
@@ -0,0 +1,7 @@
+package com.algaworks.osworks.domain.model;
+
+public enum SituacaoOrdemServico {
+	
+	ABERTA, FINALIZADA, CANCELADA
+
+}
---
+++ src/main/java/com/algaworks/osworks/domain/service/GestaoOrdemServicoService.java
@@ -0,0 +1,35 @@
+package com.algaworks.osworks.domain.service;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.model.SituacaoOrdemServico;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
+
+@Service
+public class GestaoOrdemServicoService {
+	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public OrdemServico criar(OrdemServico ordemServico) throws Exception {
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new Exception("Cliente não encontrado."));
+		
+		ordemServico.setCliente(cliente);
+		ordemServico.setSituacao(SituacaoOrdemServico.ABERTA);
+		ordemServico.setDataAbertura(OffsetDateTime.now());
+		
+		return ordemServicoRepository.save(ordemServico);
+	}
+
+}
